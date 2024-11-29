865e10ac9fa3dff14f7625756ddc25ac2c10b926
omanwhatiscomputer <khubayeebkabeer@gmail.com>
2024-11-29 20:39:31 +0600

    fix:cors1

--- app/api/books/[locale]/[seed]/[likes]/[reviews]/route.js
+++ app/api/books/[locale]/[seed]/[likes]/[reviews]/route.js
@@ -179,6 +179,15 @@ export const GET = async (request, props) => {
         status: 200,
     });
 
+    const origin = request.headers.get("origin") || "*";
+    response.headers.set("Access-Control-Allow-Origin", origin);
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    response.headers.set(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, itoken"
+    );
+    response.headers.set("Access-Control-Allow-Credentials", "true");
+
     response.headers.set("itoken", itoken);
 
     return response;
