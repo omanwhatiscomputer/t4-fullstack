60c27b25834d9f5641f25b0308f8d035fc24e200
omanwhatiscomputer <khubayeebkabeer@gmail.com>
2024-11-29 23:39:43 +0600

    fixed:caching issue3

--- app/(main)/ViewBooks.js
+++ app/(main)/ViewBooks.js
@@ -193,6 +193,13 @@ export default function ViewBooks(props) {
         };
     }, [inView]);
 
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            window.location.reload();
+            window.scrollTo(0, 0);
+        }
+    }, []);
+
     const tHeadSticky = "sticky top-[6.7rem] z-10 bg-slate-50";
     const tHeadBorderStyle = "sticky top-[9.2rem] z-10 bg-black";
     const tHeadMarginBottomStyle = "h-[100px]";
